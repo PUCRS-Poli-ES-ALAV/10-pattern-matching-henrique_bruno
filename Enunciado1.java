03ec3e9b98870f639ccacb0e3518658c7521e0a5
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2023-05-09 18:11:21 -0300

    Correção static

--- Enunciado1.java
+++ Enunciado1.java
@@ -45,7 +45,7 @@ public class Enunciado1 {
         return N; // nenhuma ocorrência
     }
 
-    private long hash(String s, int M) {
+    private static long hash(String s, int M) {
         long h = 0;
         for (int j = 0; j < M; j++)
            h = (h * R + s.charAt(j)) % Q;
